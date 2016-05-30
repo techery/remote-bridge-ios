53760745192ea0f96e15d9a2211faab185ed2536
Sergey Zenchenko <zen@sergeys-macbook-12.local>
2016-05-30 11:09:37 +0300

    Initial Commit

---
+++ RemoteBridge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RemoteBridge
+//
+//  Created by Sergey Zenchenko on 5/30/16.
+//  Copyright © 2016 Techery. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
